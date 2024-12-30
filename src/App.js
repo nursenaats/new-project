994a03704323ad5d329bc3188bbdf40d20d33f49
nursena <nursenaatasal101@gmail.com>
2024-12-30 19:58:24 +0300

    Yeni degisiklik

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy Github Kursuna Hoşgeldiniz!
+          Git & Github Ogreniyorum w/tofi
         </p>
         <a
           className="App-link"
